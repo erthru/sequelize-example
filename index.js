1d8b39f4f98a1c1c0ea7ac62103a2a4acb57c4d9
Anto D <ersaka96@gmail.com>
2023-01-14 21:26:30 +0800

    Finalizing

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+require("./configs/env");
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const routes = require("./routes");
 const db = require("./configs/db");
 
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
